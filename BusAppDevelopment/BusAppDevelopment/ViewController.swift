2ff72daaa029fe5ae4a55cc454534cf2f91c4aae
meintgl <55160992+meintgl@users.noreply.github.com>
2019-11-18 15:56:57 -0600

    Add files via upload

---
+++ BusAppDevelopment/BusAppDevelopment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusAppDevelopment
+//
+//  Created by Meinhard Benedict Capucao on 11/15/19.
+//  Copyright © 2019 Meinhard Benedict Capucao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
