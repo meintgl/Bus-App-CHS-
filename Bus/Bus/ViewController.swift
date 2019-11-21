86d645e81f4ce931338d02a3b788b1c231bd97e9
Jun Chen <55160562+jjdsjk@users.noreply.github.com>
2019-11-21 15:31:24 -0600

    Add files via upload
    Admin side but I have no idea what to do

---
+++ Bus/Bus/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Bus
+//
+//  Created by Jun Chen on 11/21/19.
+//  Copyright © 2019 Jun Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var busLocations: UIImageView!
+    
+    @IBAction func addBus(_ sender: UIButton) {
+        
+    }
+ 
+}
