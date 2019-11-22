10f2dd87dcd05b8be9bb364e2af638c52b39fa99
Jun Chen <55160562+jjdsjk@users.noreply.github.com>
2019-11-22 16:06:53 -0600

    settings page

---
+++ settingspage/settingspage/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  settingspage
+//
+//  Created by Jun Chen on 11/22/19.
+//  Copyright © 2019 Jun Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var backgroundColor: UIImageView!
+    @IBOutlet weak var titleArea: UIStackView!
+    
+    @IBOutlet weak var profileOne: UIImageView!
+    
+}
+
